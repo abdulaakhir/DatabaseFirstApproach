ea7d5aa923ddea18d42092e2cb5805e8a61581a3
abdul <abdulanm@gmail.com>
2024-04-17 02:51:56 +0530

    Add project files.

---
+++ Abdul43/Models/CodeFirstDb23Context.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abdul43.Models;
+
+public partial class CodeFirstDb23Context : DbContext
+{
+    public CodeFirstDb23Context()
+    {
+    }
+
+    public CodeFirstDb23Context(DbContextOptions<CodeFirstDb23Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employe> Employes { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employe>(entity =>
+        {
+            entity.ToTable("Employe");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Designation)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Abdul43/Models/Employe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abdul43.Models;
+
+public partial class Employe
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Designation { get; set; } = null!;
+
+    public int Salary { get; set; }
+}
